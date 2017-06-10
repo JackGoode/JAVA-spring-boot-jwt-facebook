9635f21eca753b52d2601deca7866367a14ee29f
eugene <eagulyi@gmail.com>
2017-06-10 14:24:54 +0300

    Removed joda time dependency

--- src/main/java/com/eagulyi/security/model/token/JwtTokenFactory.java
+++ src/main/java/com/eagulyi/security/model/token/JwtTokenFactory.java
@@ -1,23 +1,19 @@
 package com.eagulyi.security.model.token;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.eagulyi.security.config.JwtSettings;
 import com.eagulyi.security.model.Scopes;
 import com.eagulyi.security.model.UserContext;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenFactory {
@@ -39,13 +35,11 @@ public class JwtTokenFactory {
         Claims claims = Jwts.claims().setSubject(userContext.getUsername());
         claims.put("scopes", userContext.getAuthorities().stream().map(Object::toString).collect(Collectors.toList()));
 
-        DateTime currentTime = new DateTime();
-
         String token = Jwts.builder()
           .setClaims(claims)
           .setIssuer(settings.getTokenIssuer())
-          .setIssuedAt(currentTime.toDate())
-          .setExpiration(currentTime.plusDays(settings.getTokenExpirationTime()).toDate())
+                .setIssuedAt(Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)))
+                .setExpiration(Date.from(LocalDateTime.now().plusDays(settings.getTokenExpirationTime()).toInstant(ZoneOffset.UTC)))
           .signWith(SignatureAlgorithm.HS512, settings.getTokenSigningKey())
         .compact();
 
@@ -57,17 +51,15 @@ public class JwtTokenFactory {
             throw new IllegalArgumentException("Cannot create JWT Token without username");
         }
 
-        DateTime currentTime = new DateTime();
-
         Claims claims = Jwts.claims().setSubject(userContext.getUsername());
         claims.put("scopes", Arrays.asList(Scopes.REFRESH_TOKEN.authority()));
-        
+
         String token = Jwts.builder()
           .setClaims(claims)
           .setIssuer(settings.getTokenIssuer())
           .setId(UUID.randomUUID().toString())
-          .setIssuedAt(currentTime.toDate())
-          .setExpiration(currentTime.plusDays(settings.getRefreshTokenExpTime()).toDate())
+                .setIssuedAt(Date.from(LocalDateTime.now().toInstant(ZoneOffset.UTC)))
+                .setExpiration(Date.from(LocalDateTime.now().plusDays(settings.getRefreshTokenExpTime()).toInstant(ZoneOffset.UTC)))
           .signWith(SignatureAlgorithm.HS512, settings.getTokenSigningKey())
         .compact();
 
